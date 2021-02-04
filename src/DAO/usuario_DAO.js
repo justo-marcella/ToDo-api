740e7e40af6f0eae9e711dd2ba53562606512c36
Marcella Justo <71738159+justo-marcella@users.noreply.github.com>
2021-02-04 17:35:37 -0300

    operacoes crud em usuarios

--- src/DAO/usuario_DAO.js
+++ src/DAO/usuario_DAO.js
@@ -15,4 +15,45 @@ module.exports = class UsuarioDAO{
             })
         })
     }
+        
+    insereUsuario(usuario){
+        return new Promise ((resolve, reject)=>{
+            this.bd.all("INSERT INTO USUARIOS (NOME, EMAIL, SENHA) VALUES (?,?,?)", usuario, (error, rows)=>{
+                if(error){
+                    reject(`Erro na inserção ${error}`)
+                }else{
+                    resolve(rows)
+                }
+            })
+        })
+    }
+
+    atualizaUsuario(parametro){
+        
+        return new Promise ((resolve, reject)=>{
+            let query = "UPDATE USUARIOS SET NOME=COALESCE(?, NOME), EMAIL=COALESCE(?, EMAIL), SENHA=COALESCE(?, SENHA)  WHERE EMAIL=?"
+                this.bd.run(query, parametro, (error, rows)=>{
+                    if(error){
+                        reject("Erro na atualização do usuário")
+                    }else{
+                        resolve("Usuário atualizado com sucesso.")
+                    }
+                } )
+        })
+    }
+
+    deletaUsuario(parametroDel){
+
+        return new Promise((resolve, reject)=>{
+            let queryDel = "DELETE FROM USUARIOS WHERE EMAIL=?"
+            this.bd.run(queryDel, parametroDel, (error, rows)=>{
+                if(error){
+                    reject(`Erro ao deletar usuário ${error}`)
+                }else{
+                    resolve("Usuário deletado com sucesso")
+                }
+            })
+        })
+    }
+
 }
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -16,6 +16,37 @@ module.exports = (app, bd)=> {
         }
         })
 
+    app.post("/usuario", async  (req, res)=>{
+            
+        try{
+            const insereUsuario = await daoUsuarios.insereUsuario([req.body.nome, req.body.email, req.body.senha]);
+            res.send(`Usuário inserido com sucesso. ${insereUsuario}`)
+            }
+        catch(error){
+            res.send(error);
+            }
+    })
 
+    app.put("/usuario/:EMAIL", async  (req, res)=>{
+        let parametro = [req.body.NOME, req.body.EMAIL, req.body.SENHA, req.params.EMAIL];
+            try{
+                const alteraUsuario = await daoUsuarios.atualizaUsuario(parametro);
+                res.send(alteraUsuario)
+            }
+            catch(error){
+                res.send(error);
+            }
+    })
+
+    app.delete("/usuario/:EMAIL", async (req, res)=>{
+        let parametroDel = [req.params.EMAIL];
+        try{
+            const apagaUsuario = await daoUsuarios.deletaUsuario(parametroDel);
+            res.send(apagaUsuario)
+        }catch(error){
+            res.send(error);
+        }
+
+    })
 
 }
